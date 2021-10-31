43373d5bd38f38d29a1fa4bf53a04b9fa76f963a
Damian Kocjan <DamianoKPL@wp.pl>
2021-10-31 22:21:54 +0100

    add data to auth tokens

--- api/api/views.py
+++ api/api/views.py
@@ -4,9 +4,33 @@ from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import SignUpSerializer
 from accounts.models import User
+
+
+class TokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        token['id'] = user.id
+        token['subscribers_count'] = user.get_subscribers_count
+        token['username'] = user.username
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['email'] = user.email
+        token['date_joined'] = str(user.date_joined)
+        token['avatar'] = str(user.avatar)
+        token['background'] = str(user.background)
+        token['description'] = user.description
+
+        return token
+
+class TokenObtainPairView(TokenObtainPairView):
+    serializer_class = TokenObtainPairSerializer
 
 
 class LogoutAndBlacklistRefreshTokenForUserView(APIView):
